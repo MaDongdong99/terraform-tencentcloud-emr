650e65c013150141abf96403e57c46e9c1dc0da2
MaDongdong <donaldma@tencent.com>
2024-10-30 11:48:56 +0700

    update example

--- examples/emr/main.tf
+++ examples/emr/main.tf
@@ -1,24 +1,40 @@
 locals {
-  availability_zone = "ap-singapore-2"
-  vpc_id = "vpc-j0sqtqk7"
-  subnet_id = "subnet-p6xvdq6i"
+  availability_zones = ["ap-singapore-2"]
+  vpc_cidr = "10.0.0.0/16"
+  subnet_cidrs = ["10.0.0.0/24"]
+  name = "test-emr-tf"
   security_group_id = "sg-3fj6w1au"
+  tags = {
+    create: "terraform"
+  }
+}
+
+module "network" {
+  source  = "terraform-tencentcloud-modules/vpc/tencentcloud"
+  version = "1.1.0"
+  vpc_name         = local.name
+  vpc_cidr         = "10.0.0.0/16"
+  vpc_is_multicast = false
+  tags             = local.tags
+
+  availability_zones = local.availability_zones
+  subnet_name        = local.name
+  subnet_cidrs       = local.subnet_cidrs
+  subnet_is_multicast = false
+  subnet_tags        = local.tags
 }
 
 module "emr" {
   source = "../../modules/emr"
 
   create_cluster = true
-  availability_zone = local.availability_zone
-  cluster_name = "test-emr-tf"
-  tags = {
-    "create": "terraform",
-    "env": "dev"
-  }
+  availability_zone = local.availability_zones[0]
+  cluster_name = local.name
+  tags = local.tags
 
   vpc_settings = {
-    vpc_id = local.vpc_id
-    subnet_id = local.subnet_id
+    vpc_id = module.network.vpc_id
+    subnet_id =  module.network.subnet_id[0]
   }
 
   login_settings = {
